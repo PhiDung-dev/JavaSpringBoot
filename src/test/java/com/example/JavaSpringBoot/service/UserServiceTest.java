48d8eec42c3484d333f9ffc018ee2a9263cba4d7
Phi Dung <phidung.dev@gmail.com>
2026-08-11 16:42:23 +0700

    Isolation in Unit test

--- src/test/java/com/example/JavaSpringBoot/service/UserServiceTest.java
+++ src/test/java/com/example/JavaSpringBoot/service/UserServiceTest.java
@@ -13,6 +13,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource("/test.properties")
 public class UserServiceTest {
 
     @Autowired
